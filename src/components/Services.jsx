58c093537630d6f597403445650533b6ecffc3a3
rajeevsujakhu <rajeevsujakhau@gmail.com>
2022-07-12 09:44:57 +0545

    homepage why choose us

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div className='grid grid-cols-2 gap-4 px-[24px] py-24 bg-gradient-to-r from-cyan-500 to-blue-500'>
+    <div className='grid grid-cols-2 gap-4 px-[24px] py-24 bg-"#fafafa"'>
  <div>
-    <h1 className='text-1xl text-primary'>
+    <h1 className='text-1xl text-blue-800'>
         Why Choose Us?
     </h1>
     <h1 className='text-5xl font-bold py-2'>
@@ -16,9 +16,21 @@ const Services = () => {
   <div>
     <div className="grid grid-cols-2">
         <div className="flex flex-col justify-center items-center">
-            <p>100%</p>
-            https://dribbble.com/shots/18357466-Digital-Marketing-Agency-Website
-            </div>
+          <h3 className='text-4xl font-bold'>100%</h3>
+          <p className='pt-3 w-48 text-center'>We have 100% of client satisfaction level</p>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <h3 className='text-4xl font-bold'>280%</h3>
+          <p className='pt-3 w-48 text-center'>We have massive growth rate of 280%</p>
+        </div>
+        <div className="flex flex-col justify-center items-center pt-[40px]">
+          <h3 className='text-4xl font-bold'>No.1</h3>
+          <p className='pt-3 w-48 text-center'>We are no.1 marketing agency across america</p>
+        </div>
+        <div className="flex flex-col justify-center items-center pt-[40px]">
+          <h3 className='text-4xl font-bold'>5.0</h3>
+          <p className='pt-3 w-48 text-center'>We have rating og 5 star in top rating sites</p>
+        </div>
     </div>
 
   </div>
