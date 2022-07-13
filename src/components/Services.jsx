670973da4f5215e660fd9b92082e9848fcbc44b8
rajeevsujakhu <rajeevsujakhau@gmail.com>
2022-07-13 09:30:15 +0545

    services added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -39,17 +39,30 @@ const Services = () => {
       <div className="px-[24px] py-24 bg-sky-200 text-center flex justify-around">
         <div className=" bg-white shadow rounded-lg max-w-xs ">
           <h1 className='text-2xl font-bold py-5'>Web Design</h1>
-          <p className='px-4 pb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem laborum totam est temporibus reiciendis nostrum. Porro vitae sequi aspernatur quasi.</p>
+          <p className='px-4 pb-5 text-justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem laborum totam est temporibus reiciendis nostrum. Porro vitae sequi aspernatur quasi.</p>
         </div>
         <div className=" bg-white shadow rounded-lg max-w-xs ">
           <h1 className='text-2xl font-bold py-5'>Design studio</h1>
-          <p className='px-4 pb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem laborum totam est temporibus reiciendis nostrum. Porro vitae sequi aspernatur quasi.</p>
+          <p className='px-4 pb-5 text-justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem laborum totam est temporibus reiciendis nostrum. Porro vitae sequi aspernatur quasi.</p>
         </div>
         <div className=" bg-white shadow rounded-lg max-w-xs ">
           <h1 className='text-2xl font-bold py-5'>Contact us</h1>
-          <p className='px-4 pb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem laborum totam est temporibus reiciendis nostrum. Porro vitae sequi aspernatur quasi.</p>
+          <p className='px-4 pb-5 text-justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem laborum totam est temporibus reiciendis nostrum. Porro vitae sequi aspernatur quasi.</p>
         </div>
-        
+      </div>
+      <div className="px-[24px] pb-24 bg-sky-200 text-center flex justify-around">
+        <div className=" bg-white shadow rounded-lg max-w-xs ">
+          <h1 className='text-2xl font-bold py-5'>Web Design</h1>
+          <p className='px-4 pb-5 text-justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem laborum totam est temporibus reiciendis nostrum. Porro vitae sequi aspernatur quasi.</p>
+        </div>
+        <div className=" bg-white shadow rounded-lg max-w-xs ">
+          <h1 className='text-2xl font-bold py-5'>Design studio</h1>
+          <p className='px-4 pb-5 text-justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem laborum totam est temporibus reiciendis nostrum. Porro vitae sequi aspernatur quasi.</p>
+        </div>
+        <div className=" bg-white shadow rounded-lg max-w-xs ">
+          <h1 className='text-2xl font-bold py-5'>Contact us</h1>
+          <p className='px-4 pb-5 text-justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem laborum totam est temporibus reiciendis nostrum. Porro vitae sequi aspernatur quasi.</p>
+        </div>
       </div>
 
     </React.Fragment>
